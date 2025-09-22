0b0009a88eac5c1d68f227314c70ab4bb1c50024
VadimDun <V_ad_im3704@mail.ru>
2025-09-22 23:15:51 +0300

    task3

--- src/app/image_op.rs
+++ src/app/image_op.rs
@@ -104,8 +104,39 @@ pub fn rgb_buffer_to_blue_channel(buf: &mut Vec<u8>) {
 /// 
 /// На выходе должен быть HSV: H [0, 360], S [0, 100], V [0, 100].
 fn rgb_to_hsv(r: u8, g: u8, b: u8) -> (u16, u8, u8) {
-    // TODO
-    return (0, 0, 0);
+    let r_f = r as f32 / 255.0;
+    let g_f = g as f32 / 255.0;
+    let b_f = b as f32 / 255.0;
+    
+    let max = r_f.max(g_f.max(b_f));
+    let min = r_f.min(g_f.min(b_f));
+    let delta = max - min;
+    
+    let v = max * 100.0;
+    
+    let s = if max == 0.0 {
+        0.0
+    } else {
+        (delta / max) * 100.0
+    };
+    
+   let h = if delta == 0.0 {
+        0.0
+    } else if max == r_f {
+        if g_f >= b_f {
+            60.0 * ((g_f - b_f) / delta) + 0.0
+        }
+        else {
+            60.0 * ((g_f - b_f) / delta) + 360.0
+        }
+    } else if max == g_f {
+        60.0 * (((b_f - r_f) / delta) + 120.0)
+    } else {
+        60.0 * (((r_f - g_f) / delta) + 240.0)
+    };
+    
+
+    (h.round() as u16, s.round() as u8, v.round() as u8)
 }
 
 /// Преобразование представления цвета из HSV в RGB.
@@ -115,8 +146,28 @@ fn rgb_to_hsv(r: u8, g: u8, b: u8) -> (u16, u8, u8) {
 /// 
 /// На выходе должен быть RGB со значениями от 0 до 255.
 fn hsv_to_rgb(h: u16, s: u8, v: u8) -> (u8, u8, u8) {
-    // TODO
-    return (0, 0, 0);
+    let s_f = s as f32 / 100.0;
+    let v_f = v as f32 / 100.0;
+
+    let h_temp = h as f32 / 60.0;
+    let hi = (h_temp.floor() as u8) % 6;
+
+    let f: f32 = h_temp - (h_temp.floor());
+    let p = (v_f * (1.0 - s_f) * 255.0).round() as u8;
+    let q = (v_f * (1.0 - f * s_f) * 255.0).round() as u8;
+    let t = (v_f * (1.0 - (1.0 - f) * s_f) * 255.0).round() as u8;
+
+    let (r_prime, g_prime, b_prime) = match hi {
+        0 => (v, t, p),
+        1 => (q, v, p),
+        2 => (p, v, t),
+        3 => (p, q, v),
+        4 => (t, p, v),
+        5 => (v, p, q),
+        _ => (0, 0, 0),
+    };
+    
+    (r_prime, g_prime, b_prime)
 }
 
 /// Прибавляет ко всей картинке buf указанное значение HSV.
